bab7936c3487f163df1e85b0a5867b4d779c4593
sugarcodetime <98475696+sugarcodetime@users.noreply.github.com>
2023-09-11 20:15:16 +0700

    first commit
    test commiit content

---
+++ demo_c/demo1/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int demo1 (){
+//int main() {
+    printf("Hello World !! \n");
+    return 0;
+}
---
+++ demo_c/demo1/sum.c
@@ -0,0 +1,17 @@
+//
+// Created by dinht on 11/09/2023.
+//
+#include <stdio.h>
+
+//int sum_main() {
+     int main() {
+    int a, b, sum;
+    printf("Enter the first number: ");
+    scanf("%d", &a);
+    printf("Enter the second number: ");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("The sum is %d", sum);
+    return 0;
+}
+
